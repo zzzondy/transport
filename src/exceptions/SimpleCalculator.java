d55e983aed3260149a8d599f0bf911225adbd735
zzzondy <artemr19032006@gmail.com>
2022-02-22 19:31:03 +0300

    ex3

--- src/exceptions/SimpleCalculator.java
+++ src/exceptions/SimpleCalculator.java
@@ -8,38 +8,40 @@ import java.util.*;
 class SimpleCalculator {
     public static void main(String[] args) throws Exception {
         FileReader fr = new FileReader("src\\exceptions\\input.txt");
-        BufferedWriter fw = new BufferedWriter(new FileWriter("src\\exceptions\\output.txt"));
+        BufferedWriter fw = new BufferedWriter(new FileWriter("src\\exceptions\\output.txt", false));
         Scanner sc = new Scanner(fr);
-        String[] array = sc.nextLine().split(" ");
-        try {
-            double num1 = Double.parseDouble(array[0]), num2 = Double.parseDouble(array[2]);
-            switch (array[1]) {
-                case "-":
-                    fw.write(String.valueOf(num1 - num2));
-                    break;
-                case "+":
-                    fw.write(String.valueOf(num1 + num2));
-                    break;
-                case "*":
-                    fw.write(String.valueOf(num1 * num2));
-                    break;
-                case "/":
-                    if (num2 == 0) {
-                        throw new ArithmeticException();
-                    }
-                    else {
-                        fw.write(String.valueOf(num1 / num2));
+        while (sc.hasNextLine()) {
+            String string = sc.nextLine();
+            String[] array = string.split(" ");
+            try {
+                double num1 = Double.parseDouble(array[0]), num2 = Double.parseDouble(array[2]);
+                switch (array[1]) {
+                    case "-":
+                        fw.write(string + " = " + (num1 - num2) + "\n");
                         break;
-                    }
-                default:
-                    throw new UnsupportedOperationException();
+                    case "+":
+                        fw.write(string + " = " + (num1 + num2) + "\n");
+                        break;
+                    case "*":
+                        fw.write(string + " = " + (num1 * num2) + "\n");
+                        break;
+                    case "/":
+                        if (num2 == 0) {
+                            throw new ArithmeticException();
+                        } else {
+                            fw.write(string + " = " + (num1 / num2) + "\n");
+                            break;
+                        }
+                    default:
+                        throw new UnsupportedOperationException();
+                }
+            } catch (NumberFormatException ex) {
+                fw.write(string + " = " + "Error! Not number\n");
+            } catch (UnsupportedOperationException exception) {
+                fw.write(string + " = " + "Operation Error!\n");
+            } catch (ArithmeticException e) {
+                fw.write(string + " = " + "Error! Division by zero\n");
             }
-        } catch (NumberFormatException ex) {
-            fw.write("Error! Not number");
-        } catch (UnsupportedOperationException exception) {
-            fw.write("Operation Error!");
-        } catch (ArithmeticException e) {
-            fw.write("Error! Division by zero");
         }
         fr.close();
         fw.close();
