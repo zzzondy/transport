983faed11dc9277e2ed2ad591c86fd598edf461e
zzzondy <artemr19032006@gmail.com>
2022-02-22 18:12:30 +0300

    ex2

--- src/exceptions/SimpleCalculator.java
+++ src/exceptions/SimpleCalculator.java
@@ -1,42 +1,47 @@
 package exceptions;
 
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.*;
 
 class SimpleCalculator {
     public static void main(String[] args) throws Exception {
         FileReader fr = new FileReader("src\\exceptions\\input.txt");
+        BufferedWriter fw = new BufferedWriter(new FileWriter("src\\exceptions\\output.txt"));
         Scanner sc = new Scanner(fr);
         String[] array = sc.nextLine().split(" ");
         try {
             double num1 = Double.parseDouble(array[0]), num2 = Double.parseDouble(array[2]);
             switch (array[1]) {
                 case "-":
-                    System.out.println(num1 - num2);
+                    fw.write(String.valueOf(num1 - num2));
                     break;
                 case "+":
-                    System.out.println(num1 + num2);
+                    fw.write(String.valueOf(num1 + num2));
                     break;
                 case "*":
-                    System.out.println(num1 * num2);
+                    fw.write(String.valueOf(num1 * num2));
                     break;
                 case "/":
                     if (num2 == 0) {
                         throw new ArithmeticException();
                     }
                     else {
-                        System.out.println(num1 / num2);
+                        fw.write(String.valueOf(num1 / num2));
                         break;
                     }
                 default:
                     throw new UnsupportedOperationException();
             }
         } catch (NumberFormatException ex) {
-            System.out.println("Error! Not number");
+            fw.write("Error! Not number");
         } catch (UnsupportedOperationException exception) {
-            System.out.println("Operation Error!");
+            fw.write("Operation Error!");
         } catch (ArithmeticException e) {
-            System.out.println("Error! Division by zero");
+            fw.write("Error! Division by zero");
         }
+        fr.close();
+        fw.close();
     }
 }
